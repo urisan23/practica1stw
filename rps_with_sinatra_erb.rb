e917cddf1b01cfda3a2ae1f58ecf67facf56938b
urisan23 <urisan91@gmail.com>
2012-09-27 15:33:24 +0100

    Añadido formulario

--- rps_with_sinatra_erb.rb
+++ rps_with_sinatra_erb.rb
@@ -9,10 +9,10 @@ end
 get '/' do
     erb :index
 end
+get '/throw/' do
 
-get '/throw/:type' do
-  # the params hash stores querystring and form data
-  @player_throw = params[:type].to_sym
+  @opcion = params['option'.to_sym].downcase
+  @player_throw = @opcion.to_sym
 
   halt(403, "You must throw one of the following: '#{@throws.join(', ')}'") unless @throws.include? @player_throw
 
@@ -20,12 +20,10 @@ get '/throw/:type' do
 
   if @player_throw == @computer_throw 
     @answer = "There is a tie"
-    erb :index
   elsif @player_throw == @defeat[@computer_throw]
     @answer = "Computer wins; #{@computer_throw} defeats #{@player_throw}"
-    erb :index
   else
     @answer = "Well done. #{@player_throw} beats #{@computer_throw}"
-    erb :index
   end
+  erb :result
 end

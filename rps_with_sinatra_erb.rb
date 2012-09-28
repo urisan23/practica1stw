94a45fa9549c2016a50c3a29125da5df2df4e286
thelonelywolf88@gmail.com <thelonelywolf88@gmail.com>
2012-09-28 12:16:46 +0100

    Actualizaciones

--- rps_with_sinatra_erb.rb
+++ rps_with_sinatra_erb.rb
@@ -1,16 +1,31 @@
 require 'sinatra'
 require 'erb'
 
+set :session_secret, ENV["SESSION_KEY"] || 'top secret'
+
 # before we process a route we'll set the response as plain text
 # and set up an array of viable moves that a player (and the
 # computer) can perform
+
+enable :session
+
+helpers do
+    include Rack, Utils
+    alias_method :h, 
+end
+    
+    
 before do
   @defeat = { rock: :scissors, paper: :rock, scissors: :paper}
   @throws = @defeat.keys
 end
 
-get '/' do
-    erb :index
+get '\/' do
+   erb :intro
+end
+
+get '/throw' do
+   redirect "/throw/#{@params[:play].to_sym}"
 end
 
 get '/throw/:type' do
@@ -23,25 +38,11 @@ get '/throw/:type' do
 
   if @player_throw == @computer_throw 
     @answer = "There is a tie"
-    erb :index
   elsif @player_throw == @defeat[@computer_throw]
     @answer = "Computer wins; #{@computer_throw} defeats #{@player_throw}"
-    erb :index
   else
     @answer = "Well done. #{@player_throw} beats #{@computer_throw}"
-    erb :index
   end
+  erb :index
 end
-__END__
 
-@@index
-<html>
-  <head>
-    <title>Rock Paper Scissors</title>
-  </head>
-  <body>
-    <h2> Computer chooses:  <%= @computer_throw %> </h2>
-    <h2> You choose: <%= @player_throw %> </h2>
-    <h1> <%= @answer %> </h1>
-  </body>
-</html>
